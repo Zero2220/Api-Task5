823e3f233944b590d865df36a9a2dca1993f0132
anarpi <anarpi@code.edu.az>
2024-06-10 12:11:02 +0400

    api5

---
+++ UniversityApi/Services/Services/Implementations/GroupService.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.Exceptions;
+using Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityApi.Data;
+using UniversityApi.Dtos;
+
+namespace Services.Services.Implementations
+{
+    public class GroupService : IGroupService
+    {
+        private readonly UniDatabase _UniDatabase;
+        public GroupService(UniDatabase uniDatabase) 
+        {
+             _UniDatabase = uniDatabase;
+        }
+        public int Create(CreateDto createDto)
+        {
+            if(createDto == null) throw new ArgumentNullException(nameof(createDto));
+            
+                if (_UniDatabase.Groups.Any(x => x.No == createDto.No))
+                    throw new DublicateEntityException();
+
+            Group group = new Group()
+            {
+                IsDeleted = false,
+                Limit = createDto.Limit,
+                No = createDto.No,
+            };
+
+            _UniDatabase.Groups.Add(group);
+            _UniDatabase.SaveChanges();
+
+            return group.Id;
+        }
+
+        public int Delete(int id)
+        {
+           if(id < 0) throw new InvalidIdEntityException();
+
+           Group group = _UniDatabase.Groups.FirstOrDefault(x => x.Id == id);
+
+            group.IsDeleted = true;
+
+            _UniDatabase.Update(group);
+            _UniDatabase.SaveChanges();
+
+            return group.Id;
+        }
+
+        public int Edit(int id ,EditDto editDto)
+        {
+
+            if (editDto == null) throw new ArgumentNullException();
+
+            if(editDto == null)throw new ArgumentNullException(nameof(editDto));
+
+            var existGroup = _UniDatabase.Groups.FirstOrDefault(x=>x.Id == id);
+
+            if(editDto.No == existGroup.No && _UniDatabase.Groups.Any(x => x.Id == id))
+            throw new DublicateEntityException(editDto.No);
+            
+
+            existGroup.No = editDto.No;
+            existGroup.Limit = editDto.Limit;
+            
+
+            _UniDatabase.Update(existGroup);
+            _UniDatabase.SaveChanges();
+            return existGroup.Id;
+        }
+
+        public List<GroupGetDto> GetAll()
+        {
+            List<Group> groups = _UniDatabase.Groups.ToList();
+
+            List<GroupGetDto> groupsGetList = groups.Select(x => new GroupGetDto
+            {
+                No = x.No,
+                Limit = x.Limit
+            }).ToList();
+
+            return groupsGetList;
+        }
+
+        public GroupGetDto GetById(int id)
+        {
+            var data = _UniDatabase.Groups.Where(x => x.IsDeleted == false).FirstOrDefault(x => x.Id == id);
+
+            if (data == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            GroupGetDto dto = new GroupGetDto
+            {
+                No = data.No,
+                Limit = data.Limit
+            };
+            return (dto);
+        }
+    }
+}
---
+++ UniversityApi/UniversityApi/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using UniversityApi.Data;
+using UniversityApi.Dtos;
+using Services.Services.Implementations;
+using Services.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Diagnostics;
+using Services.Exceptions;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("~/wwwroot/logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+
+
+
+builder.Services.AddDbContext<UniDatabase>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"),
+        sqlOptions => sqlOptions.MigrationsAssembly("UniversityApi"));
+});
+
+
+
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateDto>();
+
+builder.Services.AddFluentValidationRulesToSwagger();
+
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IGroupService, GroupService>();
+builder.Services.AddScoped<IStudentService, StudentService>();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseMiddleware<SerilogMiddleware>();
+
+app.Run();
